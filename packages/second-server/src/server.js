96fbd51956c3d46a55e07475a8548580040329e6
Marcelo Ivo <marceloimgontijo@gmail.com>
2020-12-11 23:32:53 -0300

    add smile face

--- packages/second-server/src/server.js
+++ packages/second-server/src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = process.env.APP_PORT || 3001
 
-app.get('/', (req,res) => res.send('<h1>Hello World from <span style="color: red">SECOND</span> Server!</h1>'))
+app.get('/', (req,res) => res.send('<h1>Hello World from <span style="color: red">SECOND</span> Server! :D</h1>'))
 
 app.listen(port, () => {
     console.log(`Second server up! Port: ${port} At: ${new Date()}`)
