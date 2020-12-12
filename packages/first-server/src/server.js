c48859458398f2b6152cf4a9e7a37acf1770eb62
Marcelo Ivo <marceloimgontijo@gmail.com>
2020-12-11 22:57:19 -0300

    instanciate server

---
+++ packages/first-server/src/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express();
+const port = process.env.APP_PORT || 3000
+
+app.get('/', (req, res) => res.send('<h1>Hello World from <span style="color: blue">FIRST</span> Server!</h1>'))
+
+app.listen(port, () => {
+    console.log(`First server up! Port: ${port} At: ${new Date()}`)
+})
---
+++ packages/second-server/src/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express();
+const port = process.env.APP_PORT || 3001
+
+app.get('/', (req,res) => res.send('<h1>Hello World from <span style="color: red">SECOND</span> Server!</h1>'))
+
+app.listen(port, () => {
+    console.log(`Second server up! Port: ${port} At: ${new Date()}`)
+})
