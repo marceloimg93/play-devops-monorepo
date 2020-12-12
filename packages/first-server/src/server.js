a5708fa8f77433b66be4aa173cdc131cea753a7a
Marcelo Ivo <marceloimgontijo@gmail.com>
2020-12-11 23:42:52 -0300

    add sad face

--- packages/first-server/src/server.js
+++ packages/first-server/src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = process.env.APP_PORT || 3000
 
-app.get('/', (req, res) => res.send('<h1>Hello World from <span style="color: blue">FIRST</span> Server!</h1>'))
+app.get('/', (req, res) => res.send('<h1>Hello World from <span style="color: blue">FIRST</span> Server! :(</h1>'))
 
 app.listen(port, () => {
     console.log(`First server up! Port: ${port} At: ${new Date()}`)
